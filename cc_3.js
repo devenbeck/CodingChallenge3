39af2a7aa8300c946085929e69c714d5d0eafcf7
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-02 16:05:49 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+// Product Price Management Scenario : 
+// Online store you are managing a product pricing system
+let prices = [50, 55, 25, 75, 100] //prices array
+prices.push(10); //add/push a new price to the end of array
+prices.shift(); // removes the first price from array
+console.log("Updated Price List: ", prices); //print new array
+
